6ba109884d1aeca477a109d7ea9fcf61dd367097
Tanawat_Chanhom <tae_chanhom@hotmail.com>
2020-11-16 17:09:14 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/home" component={Home}></Route>
-        </Switch>
-      </Router>
+      <Home />
     </div>
   );
 }
